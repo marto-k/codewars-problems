32826d82587c0b37295470481e1069a98ca2e0a8
martin <martow17@gmail.com>
2016-02-12 18:42:14 +0300

    add git

--- javascript/Calculator.js
+++ javascript/Calculator.js
@@ -1,11 +1,11 @@
-#Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+/**Let's build a calculator that can calculate the average for an arbitrary number of arguments.
 
-#The test expects you to provide a Calculator object with an average method:
+The test expects you to provide a Calculator object with an average method:
 
-#Calculator.average()
-#The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+Calculator.average()
+The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
 
-#It expects Calculator.average(3,4,5) to return 4. 
+It expects Calculator.average(3,4,5) to return 4. */
 
 sol:
   
