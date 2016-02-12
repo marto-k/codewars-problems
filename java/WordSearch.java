9d5e31c8b078ea20e93cbee2f367bfe8ae3117f7
martin <martow17@gmail.com>
2016-02-12 18:45:36 +0300

    add git

--- java/WordSearch.java
+++ java/WordSearch.java
@@ -8,3 +8,19 @@ If the string isn't contained in any of the strings in the array, the method ret
 Examples
 
 If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].*/
+
+sol:
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class WordSearch {
+  static String[] findWord(String x, String[] y){
+        List<String> list = Arrays.stream(y).filter(s -> s.toLowerCase().contains(x)).collect(Collectors.toList());
+        if (list.isEmpty()) {
+            list.add("Empty");
+        }
+        return list.toArray(new String[0]);
+  }
+}
