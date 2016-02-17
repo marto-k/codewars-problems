9bbe332501795445d5b27057c6108084fa210178
martin <martow17@gmail.com>
2016-02-18 01:50:29 +0300

    add python problem

--- python/InsertDashes.py
+++ python/InsertDashes.py
@@ -1,1 +1,8 @@
- 
+#Write a function insertDash(num) that will insert dashes ('-') between each two odd numbers in num. For example: #if num is 454793 the output should be 4547-9-3. Don't count zero as an odd number. 
+
+
+import re
+
+def insert_dash(num):
+    #your code here
+    return re.sub(r'([13579])(?=[13579])', r'\1-', str(num))
