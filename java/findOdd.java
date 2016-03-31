9fa13b522de3055a8a1b1ec0557143a2262803b2
martin <martow17@gmail.com>
2016-03-31 11:12:55 -0800

    findOdd.java

--- java/findOdd.java
+++ java/findOdd.java
@@ -28,4 +28,12 @@ or
 
 
 
-
+public class FindOdd {
+  public static int findIt(int[] A) {
+    int xor = 0;
+    for (int i = 0; i < A.length; i++) {
+      xor ^= A[i];
+    }
+    return xor;
+  }
+}
