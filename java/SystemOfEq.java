80413abcc97744a8e7f9f993f8693815306eda5c
martin <martow17@gmail.com>
2016-03-31 11:18:46 -0800

    SystemOfEq.java

---
+++ java/SystemOfEq.java
@@ -0,0 +1,19 @@
+//Vasya isn't really good at math. However, he wants to get a good mark for the class. So he made a deal with his teacher. "I wil study very hard and will be able to solve any given problem!" - Vasya said.
+
+//Finally, today is the time to show what Vasya achieved. He solved the given task immediately. Can you?
+
+//Task:
+
+//You are given a system of equations:
+
+
+
+//In JS, C# and Java the parameters of the system: 1 ≤ n, m ≤ 1000
+
+//You should count, how many there are pairs of integers (a, b) (0 ≤ a, b) which satisfy the system.
+
+//Examples
+
+//SystemOfEq.Solution(9,3) // => 1 
+//SystemOfEq.Solution(14,28) // => 1
+//SystemOfEq.Solution(4,20) // => 0 
