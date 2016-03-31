c178862ecff431a540596630c549e972bc60fb53
martin <martow17@gmail.com>
2016-03-31 11:11:39 -0800

    findOdd.java

---
+++ java/findOdd.java
@@ -0,0 +1,31 @@
+//Given an array, find the int that appears an odd number of times.
+
+//There will always be only one integer that appears an odd number of times. 
+
+
+Solution:
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindOdd {
+
+    public static int findIt(final int[] A) {
+
+        final List<Integer> collection = IntStream.of(A).mapToObj(Integer::valueOf).collect(Collectors.toList());
+        return IntStream.of(A).filter(x -> Collections.frequency(collection, x) % 2 == 1).findFirst().getAsInt();
+    }
+}
+
+
+
+or
+
+
+
+
+
+
+
